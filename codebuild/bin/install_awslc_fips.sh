eaa2dc77d80ab420c3aabe7ddebc2b9b80625e07
Carol Yeh <69044490+CarolYeh910@users.noreply.github.com>
2026-03-19 22:36:58 +0000

    bump awslc release page size

--- codebuild/bin/install_awslc_fips.sh
+++ codebuild/bin/install_awslc_fips.sh
@@ -49,18 +49,18 @@ check_dep go
 BUILD_DIR=$1
 INSTALL_DIR=$2
 VERSION=$3
-GH_RELEASE_URL="https://api.github.com/repos/aws/aws-lc/releases"
+GH_RELEASE_URL="https://api.github.com/repos/aws/aws-lc/releases?per_page=100"
 
 # Map version to the latest release of the certificate year.
 # Note: since the next FIPS validation will be split off from main
 # building main with FIPS enabled is the next candidate FIPS branch.
 case $VERSION in
   "2022")
-    AWSLC_BRANCH=$(curl --silent $GH_RELEASE_URL \
+    AWSLC_BRANCH=$(curl $GH_RELEASE_URL \
       |grep -Po '"tag_name": "\KAWS-LC-FIPS-2.*?(?=")' |head -1)
     ;;
   "2024")
-    AWSLC_BRANCH=$(curl --silent $GH_RELEASE_URL \
+    AWSLC_BRANCH=$(curl $GH_RELEASE_URL \
       |grep -Po '"tag_name": "\KAWS-LC-FIPS-3.*?(?=")' |head -1)
     ;;
   "next")
